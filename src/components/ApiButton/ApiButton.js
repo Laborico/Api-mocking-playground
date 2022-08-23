00d3013d528818ad9aa21465c5bb7c228e1e77e5
David Laborico <david.laborico@michelada.io>
2022-08-22 23:31:53 -0500

    Added alerts for status codes

--- src/components/ApiButton/ApiButton.js
+++ src/components/ApiButton/ApiButton.js
@@ -1,15 +1,72 @@
-import { Row, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Row, Button, Alert } from 'react-bootstrap';
 import { apiCall } from '../../services/api_call';
 import './ApiButton.css';
 
 function ApiButton(){
 
+    const [show100, setShow100] = useState(false);
+    const [show200, setShow200] = useState(false);
+    const [show300, setShow300] = useState(false);
+    const [show400, setShow400] = useState(false);
+    const [show500, setShow500] = useState(false);
+
+    const handleResponse = async (e) => {
+        let response = await apiCall();
+        if(response.status >= 500) {
+            setShow500(true);
+        } else if (response.status >= 400){
+            setShow400(true);
+        } else if (response.status >= 300){
+            setShow300(true);
+        } else if (response.status >= 200){
+            setShow200(true);
+        } else if (response.status >= 0){
+            setShow100(true);
+        }
+    }
+
     return(
-        <Row className="center">
-            <Button type="submit" variant="primary" onClick={apiCall}>
+        <>
+        <Row>
+            { show500 ? 
+                <Alert variant="danger" onClose={() => setShow500(false)} dismissible>
+                    Se nos cayo el changarro! Lo sentimos, intenta despues:C
+                </Alert>
+                : null 
+            } 
+            { show400 ? 
+                <Alert variant="warning" onClose={() => setShow400(false)} dismissible>
+                    Tuviste un error, revisa la informacion:D
+                </Alert>
+                : null 
+            } 
+            { show300 ? 
+                <Alert variant="primary" onClose={() => setShow300(false)} dismissible>
+                    El endpoint fue movido, por favor revisa la ruta:D 
+                </Alert>
+                : null 
+            } 
+            { show200 ? 
+                <Alert variant="success" onClose={() => setShow200(false)} dismissible>
+                    Todo salio bien:D
+                </Alert>
+                : null 
+            } 
+            { show100 ? 
+                <Alert variant="secondary" onClose={() => setShow100(false)} dismissible>
+                    El servidor esta procesando la informacion, por favor espera:D
+                </Alert>
+                : null 
+            } 
+        </Row>
+        <Row className="center row_button">
+            <Button type="submit" variant="primary" onClick={handleResponse}>
                 Llamar a la API
             </Button>
         </Row> 
+        </>
+
     );
 }
 
--- src/services/api_call.js
+++ src/services/api_call.js
@@ -5,9 +5,9 @@ export async function apiCall() {
     try{
         const response = await axios.post('/api/v1/endpoint');
         console.log('response  ', response)
-        return response.data;
+        return response;
     }catch(error) {
-        return [];
+        return error.response;
     }
     
 }
